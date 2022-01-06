96ed5172398d92548e3c465f4d0a905c503ce612
Joshua <39704584+Bloodimooni@users.noreply.github.com>
2022-01-06 17:26:11 +0100

    Create go_func.go
    random test for go functions

---
+++ go_func.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// main function
+
+	fmt.Println("\n", square(15125), "\n", root(64))
+
+	for i := 0; i < 200000; i++ {
+		fmt.Println(make_string(rune(i)), " ", i)
+	}
+
+}
+
+func square(value float64) float64 {
+	return value * value
+}
+
+func root(value float64) float64 {
+	return math.Sqrt(value)
+}
+
+func make_string(value rune) string {
+	return string(value)
+}
